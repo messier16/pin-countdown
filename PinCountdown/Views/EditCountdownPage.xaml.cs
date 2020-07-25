3ec177fb1ba559a05996989f07acffff6a09f992
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-07-25 21:39:47 +0100

    don't fail on cancel

--- PinCountdown/Views/EditCountdownPage.xaml.cs
+++ PinCountdown/Views/EditCountdownPage.xaml.cs
@@ -21,27 +21,28 @@ namespace PinCountdown.Views
         async void ImageCell_Tapped(System.Object sender, System.EventArgs e)
         {
 
-            var status = await Permissions.RequestAsync<Permissions.Media>();
+            var status = await Permissions.RequestAsync<Permissions.Photos>();
 
             if(status == PermissionStatus.Granted)
             {
-                // Supply media options for saving our photo after it's taken.
                 var mediaOptions = new Plugin.Media.Abstractions.PickMediaOptions
                 {
                     PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
                 };
 
-                // Take a photo of the business receipt.
                 var file = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
 
-                var vm = BindingContext as EditCountdownViewModel;
+                if (file != null)
+                {
+                    var vm = BindingContext as EditCountdownViewModel;
 
-                byte[] bytes = null;
-                var stream = file.GetStream();
-                bytes = new byte[stream.Length];
-                stream.Read(bytes, 0, (int)stream.Length);
-                stream.Seek(0, System.IO.SeekOrigin.Begin);
-                ViewModel.ImageBytes = bytes;
+                    byte[] bytes = null;
+                    var stream = file.GetStream();
+                    bytes = new byte[stream.Length];
+                    stream.Read(bytes, 0, (int)stream.Length);
+                    stream.Seek(0, System.IO.SeekOrigin.Begin);
+                    ViewModel.ImageBytes = bytes;
+                }
             }
         }
     }
